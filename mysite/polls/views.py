c740cee1b24afba9c8c4a91cd09dd721769f1582
suhanar <suhanasiraj60@gmail.com>
2020-07-29 23:59:03 -0400

    commiting polls

---
+++ mysite/polls/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import Http404
+#shortcut for 404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from .models import Question,Choice
+from django.urls import reverse
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def index(request):
+
+    latest_question_lists = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_lists' : latest_question_lists
+    }
+
+    return HttpResponse(template.render(context,request))
+    #shortcut for rendering
+
+    #return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question =Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question not found")
+    return render(request,'polls/details.html',{'question':question})
+    #shortcut for Http404
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/detail.html', {'question': question})
+
+
+
+    #return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
